736c1e6d0dfda03497d9e12857e838ec31453157
Rayen Sbai <rayensbai2@gmail.com>
2026-04-19 10:33:19 +0100

    chore: remove streams

--- src/render/canvas.ts
+++ src/render/canvas.ts
@@ -6,7 +6,6 @@ import { drawWaveform } from './sections/waveform.js'
 import { drawMilestones } from './sections/milestones.js'
 import { drawStats } from './sections/stats.js'
 import { drawLanguages } from './sections/languages.js'
-import { drawStreams } from './sections/streams.js'
 import { drawContributorList } from './sections/contributorList.js'
 import { drawFooter } from './sections/footer.js'
 
@@ -25,7 +24,6 @@ export async function render(data: RepoData, theme: Theme): Promise<Buffer> {
   y = drawMilestones(ctx, data, theme, y)
   y = drawStats(ctx, data, theme, y)
   y = drawLanguages(ctx, data, theme, y)
-  y = drawStreams(ctx, data, theme, y)
   y = drawContributorList(ctx, data, theme, y)
   drawFooter(ctx, data, theme)
 
